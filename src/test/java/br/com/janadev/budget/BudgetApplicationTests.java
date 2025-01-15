f067b679752df4e240fd8982e482adee1c473591
Janaina Paula <janainadepaula93@gmail.com>
2025-01-15 17:04:33 -0300

    primeiro commit

---
+++ src/test/java/br/com/janadev/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.janadev.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
