2e2f577d404740bed1af0af86a2902e803a744af
Janaina Paula <janainadepaula93@gmail.com>
2025-01-24 10:21:42 -0300

    implementando regra de não ter receitas com mesmo título na atualização de receita

--- src/main/java/br/com/janadev/budget/domain/income/usecases/UpdateIncomeUseCase.java
+++ src/main/java/br/com/janadev/budget/domain/income/usecases/UpdateIncomeUseCase.java
@@ -1,11 +1,13 @@
 package br.com.janadev.budget.domain.income.usecases;
 
 import br.com.janadev.budget.domain.exceptions.DomainNotFoundException;
+import br.com.janadev.budget.domain.exceptions.IncomeAlreadyExistsException;
 import br.com.janadev.budget.domain.income.Income;
 import br.com.janadev.budget.domain.income.commands.IncomeCommand;
 import br.com.janadev.budget.domain.income.ports.primary.UpdateIncomePort;
 import br.com.janadev.budget.domain.income.ports.secondary.IncomeDatabasePort;
 
+import static br.com.janadev.budget.domain.exceptions.IncomeErrorMessages.INCOME_WITH_THIS_DESCRIPTION_ALREADY_EXISTS;
 import static br.com.janadev.budget.domain.exceptions.IncomeErrorMessages.UPDATE_FAILED_INCOME_NOT_FOUND;
 
 public class UpdateIncomeUseCase implements UpdateIncomePort {
@@ -22,8 +24,18 @@ public class UpdateIncomeUseCase implements UpdateIncomePort {
         if (income == null){
             throw new DomainNotFoundException(UPDATE_FAILED_INCOME_NOT_FOUND);
         }
+
+        if(isChangeDescription(incomeCommand, income)){
+            if (incomeDatabasePort.descriptionAlreadyExists(incomeCommand.getDescription())){
+                throw new IncomeAlreadyExistsException(INCOME_WITH_THIS_DESCRIPTION_ALREADY_EXISTS);
+            }
+        }
         return incomeDatabasePort.updateById(
                 Income.of(income.getId(), incomeCommand.getDescription(), incomeCommand.getAmount(), incomeCommand.getDate())
         );
+    }
+
+    private static boolean isChangeDescription(IncomeCommand incomeCommand, Income income) {
+        return !income.getDescription().equals(incomeCommand.getDescription());
     }
 }
