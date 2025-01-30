c7e4257210464449dee9f170288f854bdd5439a3
Janaina Paula <janainadepaula93@gmail.com>
2025-01-30 08:38:36 -0300

    Adicionando GetExpenseDetailsUseCase como um bean do Spring

--- src/main/java/br/com/janadev/budget/secondary/expense/config/ExpenseDatabaseConfig.java
+++ src/main/java/br/com/janadev/budget/secondary/expense/config/ExpenseDatabaseConfig.java
@@ -1,6 +1,7 @@
 package br.com.janadev.budget.secondary.expense.config;
 
 import br.com.janadev.budget.domain.expense.ports.ExpenseDatabasePort;
+import br.com.janadev.budget.domain.expense.usecases.GetExpenseDetailsUseCase;
 import br.com.janadev.budget.domain.expense.usecases.RegisterExpenseUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +18,10 @@ public class ExpenseDatabaseConfig {
     @Bean
     public RegisterExpenseUseCase registerExpenseUseCase(){
         return new RegisterExpenseUseCase(expenseDatabasePort);
+    }
+
+    @Bean
+    public GetExpenseDetailsUseCase getExpenseDetailsUseCase(){
+        return new GetExpenseDetailsUseCase(expenseDatabasePort);
     }
 }
