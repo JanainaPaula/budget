2b2cabd98d1cd3970f5dbff96d3511f31f8005eb
Janaina Paula <janainadepaula93@gmail.com>
2025-02-04 08:47:43 -0300

    Criando interface FindAllExpensePort

---
+++ src/main/java/br/com/janadev/budget/domain/expense/ports/primary/FindAllExpensePort.java
@@ -0,0 +1,9 @@
+package br.com.janadev.budget.domain.expense.ports.primary;
+
+import br.com.janadev.budget.domain.expense.Expense;
+
+import java.util.List;
+
+public interface FindAllExpensePort {
+    List<Expense> findAll();
+}
