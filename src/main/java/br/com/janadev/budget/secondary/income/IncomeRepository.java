31811833195fc92869625b93754f953daa5b7c38
Janaina Paula <janainadepaula93@gmail.com>
2025-02-14 08:22:11 -0300

    adicionando método findByDescription em IncomeRepository

--- src/main/java/br/com/janadev/budget/secondary/income/IncomeRepository.java
+++ src/main/java/br/com/janadev/budget/secondary/income/IncomeRepository.java
@@ -3,7 +3,9 @@ package br.com.janadev.budget.secondary.income;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<IncomeDBO, Long> {
     boolean existsByDescriptionAndDateBetween(String description, LocalDate start, LocalDate end);
+    List<IncomeDBO> findByDescriptionContainingIgnoreCase(String description);
 }
