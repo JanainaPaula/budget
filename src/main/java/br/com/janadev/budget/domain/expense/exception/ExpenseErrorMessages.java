465060e3ecc81057e91150a7391474462d12922a
Janaina Paula <janainadepaula93@gmail.com>
2025-02-13 21:14:10 -0300

    mudando ExpenseErrorMessages para um enum

--- src/main/java/br/com/janadev/budget/domain/expense/exception/ExpenseErrorMessages.java
+++ src/main/java/br/com/janadev/budget/domain/expense/exception/ExpenseErrorMessages.java
@@ -1,16 +1,26 @@
 package br.com.janadev.budget.domain.expense.exception;
 
-public class ExpenseErrorMessages {
+import br.com.janadev.budget.domain.exceptions.ErrorMessages;
 
-    public static final String EXPENSE_DESCRIPTION_CANNOT_BE_NULL = "Description cannot be null or empty.";
-    public static final String EXPENSE_AMOUNT_MUST_BE_GREATER_THAN_ZERO = "Amount must be greater than zero.";
-    public static final String EXPENSE_DATE_CANNOT_BE_NULL = "Date cannot be null.";
-    public static final String EXPENSE_DESCRIPTION_ALREADY_EXIST = "Already exist an expence with this description.";
-    public static final String EXPENSE_NOT_FOUND = "Expense not found.";
-    public static final String EXPENSE_DELETE_FAILED_NOT_FOUND = "Could not delete. Expense not found.";
-    public static final String EXPENSE_UPDATE_FAILED_NOT_FOUND = "Could not update. Expense not found.";
-    public static final String EXPENSE_CATEGORY_NOT_FOUND = "Category does not exist.";
+public enum ExpenseErrorMessages implements ErrorMessages {
 
-    private ExpenseErrorMessages() {
+    EXPENSE_DESCRIPTION_CANNOT_BE_NULL("Description cannot be null or empty."),
+    EXPENSE_AMOUNT_MUST_BE_GREATER_THAN_ZERO("Amount must be greater than zero."),
+    EXPENSE_DATE_CANNOT_BE_NULL("Date cannot be null."),
+    EXPENSE_DESCRIPTION_ALREADY_EXIST("Already exist an expence with this description."),
+    EXPENSE_NOT_FOUND("Expense not found."),
+    EXPENSE_DELETE_FAILED_NOT_FOUND("Could not delete. Expense not found."),
+    EXPENSE_UPDATE_FAILED_NOT_FOUND("Could not update. Expense not found."),
+    EXPENSE_CATEGORY_NOT_FOUND("Category does not exist.");
+
+    private final String message;
+
+    ExpenseErrorMessages(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
     }
 }
