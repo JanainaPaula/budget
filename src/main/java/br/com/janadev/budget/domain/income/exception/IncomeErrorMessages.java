b5f2dfcc5fd465d70f6323a4934898d57491729c
Janaina Paula <janainadepaula93@gmail.com>
2025-02-13 21:19:20 -0300

    Mudando IncomeErrorMessages para enum

--- src/main/java/br/com/janadev/budget/domain/income/exception/IncomeErrorMessages.java
+++ src/main/java/br/com/janadev/budget/domain/income/exception/IncomeErrorMessages.java
@@ -1,14 +1,24 @@
 package br.com.janadev.budget.domain.income.exception;
 
-public class IncomeErrorMessages {
-    public static final String INCOME_DESCRIPTION_CANNOT_BE_NULL = "Description cannot be null or empty.";
-    public static final String INCOME_AMOUNT_MUST_BE_GREATER_THAN_ZERO = "Amount must be greater than zero.";
-    public static final String INCOME_DATE_CANNOT_BE_NULL = "Date cannot be null.";
-    public static final String INCOME_NOT_FOUND = "Income not found.";
-    public static final String INCOME_UPDATE_FAILED_NOT_FOUND = "Could not update. Income not found.";
-    public static final String INCOME_DELETE_FAILED_NOT_FOUND = "Could not delete. Income not found.";
-    public static final String INCOME_WITH_THIS_DESCRIPTION_ALREADY_EXISTS = "An income with this description already exists.";
+import br.com.janadev.budget.domain.exceptions.ErrorMessages;
 
-    private IncomeErrorMessages() {
+public enum IncomeErrorMessages implements ErrorMessages {
+    INCOME_DESCRIPTION_CANNOT_BE_NULL("Description cannot be null or empty."),
+    INCOME_AMOUNT_MUST_BE_GREATER_THAN_ZERO("Amount must be greater than zero."),
+    INCOME_DATE_CANNOT_BE_NULL("Date cannot be null."),
+    INCOME_NOT_FOUND("Income not found."),
+    INCOME_UPDATE_FAILED_NOT_FOUND("Could not update. Income not found."),
+    INCOME_DELETE_FAILED_NOT_FOUND("Could not delete. Income not found."),
+    INCOME_WITH_THIS_DESCRIPTION_ALREADY_EXISTS("An income with this description already exists.");
+
+    private final String message;
+
+    IncomeErrorMessages(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
     }
 }
