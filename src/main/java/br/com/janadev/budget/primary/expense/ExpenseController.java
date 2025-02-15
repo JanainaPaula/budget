475a97b6777a674995809104d1baeed203c6c261
Janaina Paula <janainadepaula93@gmail.com>
2025-02-15 17:44:57 -0300

    Criando endpoint de busca por descrição

--- src/main/java/br/com/janadev/budget/primary/expense/ExpenseController.java
+++ src/main/java/br/com/janadev/budget/primary/expense/ExpenseController.java
@@ -4,6 +4,7 @@ import br.com.janadev.budget.domain.expense.Category;
 import br.com.janadev.budget.domain.expense.Expense;
 import br.com.janadev.budget.domain.expense.ports.primary.DeleteExpensePort;
 import br.com.janadev.budget.domain.expense.ports.primary.FindAllExpensesPort;
+import br.com.janadev.budget.domain.expense.ports.primary.FindExpenseByDescriptionPort;
 import br.com.janadev.budget.domain.expense.ports.primary.GetExpenseDetailsPort;
 import br.com.janadev.budget.domain.expense.ports.primary.RegisterExpensePort;
 import br.com.janadev.budget.domain.expense.ports.primary.UpdateExpensePort;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -31,17 +33,20 @@ public class ExpenseController {
     private final FindAllExpensesPort findAllExpensesPort;
     private final DeleteExpensePort deleteExpensePort;
     private final UpdateExpensePort updateExpensePort;
+    private final FindExpenseByDescriptionPort findExpenseByDescriptionPort;
 
     public ExpenseController(RegisterExpensePort registerExpensePort,
                              GetExpenseDetailsPort getExpenseDetailsPort,
                              FindAllExpensesPort findAllExpensesPort,
                              DeleteExpensePort deleteExpensePort,
-                             UpdateExpensePort updateExpensePort) {
+                             UpdateExpensePort updateExpensePort,
+                             FindExpenseByDescriptionPort findExpenseByDescriptionPort) {
         this.registerExpensePort = registerExpensePort;
         this.getExpenseDetailsPort = getExpenseDetailsPort;
         this.findAllExpensesPort = findAllExpensesPort;
         this.deleteExpensePort = deleteExpensePort;
         this.updateExpensePort = updateExpensePort;
+        this.findExpenseByDescriptionPort = findExpenseByDescriptionPort;
     }
 
     @PostMapping
@@ -59,13 +64,6 @@ public class ExpenseController {
         return ResponseEntity.ok(ExpenseResponseDTO.toDTO(expense));
     }
 
-    @GetMapping
-    public ResponseEntity<List<ExpenseResponseDTO>> findAll(){
-        List<ExpenseResponseDTO> expenses =
-                findAllExpensesPort.findAll().stream().map(ExpenseResponseDTO::toDTO).toList();
-        return ResponseEntity.ok(expenses);
-    }
-
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         deleteExpensePort.delete(id);
@@ -78,5 +76,27 @@ public class ExpenseController {
                 Expense.of(request.description(), request.amount(), request.date(),
                         Category.getCategoryByName(request.category())));
         return ResponseEntity.ok(ExpenseResponseDTO.toDTO(expenseUpdated));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ExpenseResponseDTO>> findAll(@RequestParam(required = false) String description){
+        if (description == null){
+            return findAll();
+        }
+        return findByDescription(description);
+
+    }
+
+    private ResponseEntity<List<ExpenseResponseDTO>> findAll() {
+        List<ExpenseResponseDTO> expenses =
+                findAllExpensesPort.findAll().stream().map(ExpenseResponseDTO::toDTO).toList();
+        return ResponseEntity.ok(expenses);
+    }
+
+    private ResponseEntity<List<ExpenseResponseDTO>> findByDescription(String description) {
+        List<ExpenseResponseDTO> expenses =
+                findExpenseByDescriptionPort.findByDescription(description)
+                        .stream().map(ExpenseResponseDTO::toDTO).toList();
+        return ResponseEntity.ok(expenses);
     }
 }
