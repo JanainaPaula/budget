55c1251fbb1caa21e0350f88353a63271ecde7e7
Janaina Paula <janainadepaula93@gmail.com>
2025-02-15 17:34:57 -0300

    adicionando método findByDescriptionContainingIgnoreCase em ExpenseRepository

--- src/main/java/br/com/janadev/budget/secondary/expense/ExpenseRepository.java
+++ src/main/java/br/com/janadev/budget/secondary/expense/ExpenseRepository.java
@@ -3,8 +3,11 @@ package br.com.janadev.budget.secondary.expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<ExpenseDBO, Long> {
 
     boolean existsByDescriptionAndDateBetween(String description, LocalDate start, LocalDate end);
+
+    List<ExpenseDBO> findByDescriptionContainingIgnoreCase(String description);
 }
