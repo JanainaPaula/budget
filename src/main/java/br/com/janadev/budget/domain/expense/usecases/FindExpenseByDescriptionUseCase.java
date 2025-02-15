f4f1109047d3fd3d412d478ed7983a30ac3d00c5
Janaina Paula <janainadepaula93@gmail.com>
2025-02-15 17:21:46 -0300

    Criando use case FindExpenseByDescription

---
+++ src/main/java/br/com/janadev/budget/domain/expense/usecases/FindExpenseByDescriptionUseCase.java
@@ -0,0 +1,21 @@
+package br.com.janadev.budget.domain.expense.usecases;
+
+import br.com.janadev.budget.domain.expense.Expense;
+import br.com.janadev.budget.domain.expense.ports.primary.FindExpenseByDescriptionPort;
+import br.com.janadev.budget.domain.expense.ports.secondary.ExpenseDatabasePort;
+
+import java.util.List;
+
+public class FindExpenseByDescriptionUseCase implements FindExpenseByDescriptionPort {
+
+    private final ExpenseDatabasePort expenseDatabasePort;
+
+    public FindExpenseByDescriptionUseCase(ExpenseDatabasePort expenseDatabasePort) {
+        this.expenseDatabasePort = expenseDatabasePort;
+    }
+
+    @Override
+    public List<Expense> findByDescription(String description) {
+        return expenseDatabasePort.findByDescription(description);
+    }
+}
