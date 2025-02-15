ae750f3c8d28818e71658d628f4e790ab0113a6a
Janaina Paula <janainadepaula93@gmail.com>
2025-02-15 19:07:24 -0300

    Criando teste unitártio para use case FindAllIncomesByMonthUseCase

---
+++ src/test/java/br/com/janadev/budget/unit/domain/income/usecases/FindAllIncomesByMonthUseCaseTest.java
@@ -0,0 +1,48 @@
+package br.com.janadev.budget.unit.domain.income.usecases;
+
+import br.com.janadev.budget.domain.income.Income;
+import br.com.janadev.budget.domain.income.ports.secondary.IncomeDatabasePort;
+import br.com.janadev.budget.domain.income.usecases.FindAllIncomesByMonthUseCase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FindAllIncomesByMonthUseCaseTest {
+
+    @Mock
+    private IncomeDatabasePort incomeDatabasePort;
+
+    @InjectMocks
+    private FindAllIncomesByMonthUseCase findAllIncomesByMonthUseCase;
+
+    @Test
+    void shouldFindAllIncomesByMonthSuccessfully(){
+        Income incomeExpected = Income.of(2L, "Salário", 5000.0,
+                LocalDate.of(2025, Month.FEBRUARY, 15));
+
+        when(incomeDatabasePort.findAllByMonth(anyInt(), anyInt())).thenReturn(List.of(incomeExpected));
+
+        List<Income> incomes = findAllIncomesByMonthUseCase.findAllByMonth(2025, 2);
+
+        assertEquals(1, incomes.size());
+        assertAll(
+                () -> assertEquals(incomeExpected.getId(), incomes.get(0).getId()),
+                () -> assertEquals(incomeExpected.getDescription(), incomes.get(0).getDescription()),
+                () -> assertEquals(incomeExpected.getAmount(), incomes.get(0).getAmount()),
+                () -> assertEquals(incomeExpected.getDate(), incomes.get(0).getDate())
+        );
+    }
+
+}
