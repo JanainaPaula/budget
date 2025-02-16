b1b92cbd57b4695a13f75b0d5eb51f7ca9f56ea2
Janaina Paula <janainadepaula93@gmail.com>
2025-02-15 23:15:05 -0300

    adicionando método findByDateYearAndDateMonth em ExpenseRepository

--- src/main/java/br/com/janadev/budget/secondary/expense/ExpenseRepository.java
+++ src/main/java/br/com/janadev/budget/secondary/expense/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package br.com.janadev.budget.secondary.expense;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,4 +11,7 @@ public interface ExpenseRepository extends JpaRepository<ExpenseDBO, Long> {
     boolean existsByDescriptionAndDateBetween(String description, LocalDate start, LocalDate end);
 
     List<ExpenseDBO> findByDescriptionContainingIgnoreCase(String description);
+
+    @Query("SELECT e FROM ExpenseDBO e WHERE YEAR(e.date) = :year AND MONTH(e.date) = :month")
+    List<ExpenseDBO> findByDateYearAndDateMonth(int year, int month);
 }
