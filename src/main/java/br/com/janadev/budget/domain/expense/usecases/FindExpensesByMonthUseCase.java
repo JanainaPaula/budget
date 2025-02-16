2aab42532fc06545418b7f6542b04185d226009c
Janaina Paula <janainadepaula93@gmail.com>
2025-02-15 23:00:48 -0300

    criando use case FindExpenseByMonthUseCase

---
+++ src/main/java/br/com/janadev/budget/domain/expense/usecases/FindExpensesByMonthUseCase.java
@@ -0,0 +1,21 @@
+package br.com.janadev.budget.domain.expense.usecases;
+
+import br.com.janadev.budget.domain.expense.Expense;
+import br.com.janadev.budget.domain.expense.ports.primary.FindExpensesByMonthPort;
+import br.com.janadev.budget.domain.expense.ports.secondary.ExpenseDatabasePort;
+
+import java.util.List;
+
+public class FindExpensesByMonthUseCase implements FindExpensesByMonthPort {
+
+    private final ExpenseDatabasePort expenseDatabasePort;
+
+    public FindExpensesByMonthUseCase(ExpenseDatabasePort expenseDatabasePort) {
+        this.expenseDatabasePort = expenseDatabasePort;
+    }
+
+    @Override
+    public List<Expense> findAllByMonth(int year, int month) {
+        return expenseDatabasePort.findAllByMonth(year, month);
+    }
+}
