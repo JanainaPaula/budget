60dc76e60cddf71df9fbd3428f28038b6d9c56ac
Janaina Paula <janainadepaula93@gmail.com>
2025-03-05 12:25:50 -0300

    Criando UserDBO e repository

---
+++ src/main/java/br/com/janadev/budget/secondary/user/UserDBO.java
@@ -0,0 +1,21 @@
+package br.com.janadev.budget.secondary.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserDBO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String senha;
+
+    public UserDBO() {
+    }
+}
---
+++ src/main/java/br/com/janadev/budget/secondary/user/UserRepository.java
@@ -0,0 +1,6 @@
+package br.com.janadev.budget.secondary.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserDBO, Long> {
+}
