75b514dc5166768f9b2a302ee9d7c4f24b461dcb
Janaina Paula <janainadepaula93@gmail.com>
2025-03-09 16:56:03 -0300

    adicionando campo roles em UserDBO

--- src/main/java/br/com/janadev/budget/secondary/auth/user/UserDBO.java
+++ src/main/java/br/com/janadev/budget/secondary/auth/user/UserDBO.java
@@ -1,10 +1,18 @@
 package br.com.janadev.budget.secondary.auth.user;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -15,6 +23,11 @@ public class UserDBO {
     private Long id;
     private String email;
     private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
 
     public UserDBO() {
     }
