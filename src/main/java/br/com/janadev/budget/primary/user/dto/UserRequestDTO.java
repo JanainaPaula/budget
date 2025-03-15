39e0b7d93cd347324b90c8a7def2ca1e4ba9f5a3
Janaina Paula <janainadepaula93@gmail.com>
2025-03-15 14:16:51 -0300

    Adicionando anotação @NotBlank em campo email na UserRequestDTO

--- src/main/java/br/com/janadev/budget/primary/user/dto/UserRequestDTO.java
+++ src/main/java/br/com/janadev/budget/primary/user/dto/UserRequestDTO.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import java.util.Set;
 
 public record UserRequestDTO(
+        @NotBlank
         @Email(message = "Invalid email address. The email must be in the following format: format@mail.com")
         String email,
         @NotBlank(message = "Invalid password. The password must not be null or empty.")
