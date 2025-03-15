0ff050575d5d81907020ca2ee5d4b8138774de65
Janaina Paula <janainadepaula93@gmail.com>
2025-03-15 14:07:22 -0300

    criando teste integrado para endpoint de remoção de usuário

--- src/test/java/br/com/janadev/budget/integrated/user/UserIntegratedTest.java
+++ src/test/java/br/com/janadev/budget/integrated/user/UserIntegratedTest.java
@@ -4,6 +4,7 @@ import br.com.janadev.budget.integrated.config.IntegratedTestBaseConfig;
 import br.com.janadev.budget.primary.user.dto.UserRequestDTO;
 import br.com.janadev.budget.primary.user.dto.UserResponseDTO;
 import br.com.janadev.budget.secondary.auth.user.Role;
+import br.com.janadev.budget.secondary.auth.user.UserDBO;
 import br.com.janadev.budget.secondary.auth.user.service.UserServicePort;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,9 +26,6 @@ public class UserIntegratedTest extends IntegratedTestBaseConfig {
 
     @Autowired
     private UserServicePort userServicePort;
-
-    @Autowired
-    private BCryptPasswordEncoder encoder;
 
     @Test
     void shouldRegisterUserSuccessfully(){
@@ -46,5 +44,20 @@ public class UserIntegratedTest extends IntegratedTestBaseConfig {
                 () -> assertEquals(request.roles().size(), response.roles().size()),
                 () -> assertTrue(request.roles().contains(Role.USER.name()))
         );
+    }
+
+    @Test
+    void shouldDeleteUserSuccessfully(){
+        var userDBO = userServicePort.register(
+                UserDBO.of("teste1@test.com", "123456", Set.of(Role.USER.name()))
+        );
+
+        HttpEntity<String> entity = new HttpEntity<>(getAuthorizationHeader());
+
+        ResponseEntity<Void> responseEntity = restTemplate.exchange("/users/{userId}", HttpMethod.DELETE,
+                entity, Void.class, userDBO.getId());
+
+        assertEquals(204, responseEntity.getStatusCode().value());
+        assertThrows(RuntimeException.class, () -> userServicePort.getUserByUsername(userDBO.getEmail()));
     }
 }
