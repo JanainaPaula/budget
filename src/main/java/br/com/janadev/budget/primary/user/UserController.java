9340f0f1584dcb6452d92629af3ee3f0a2f7ea6f
Janaina Paula <janainadepaula93@gmail.com>
2025-03-15 16:23:25 -0300

    Criando endpoint de atualização do usuário

--- src/main/java/br/com/janadev/budget/primary/user/UserController.java
+++ src/main/java/br/com/janadev/budget/primary/user/UserController.java
@@ -2,8 +2,8 @@ package br.com.janadev.budget.primary.user;
 
 import br.com.janadev.budget.primary.user.dto.UserRequestDTO;
 import br.com.janadev.budget.primary.user.dto.UserResponseDTO;
-import br.com.janadev.budget.secondary.auth.user.UserDBO;
-import br.com.janadev.budget.secondary.auth.user.service.UserServicePort;
+import br.com.janadev.budget.primary.user.dto.UserUpdateDTO;
+import br.com.janadev.budget.secondary.user.dbo.UserDBO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,20 +11,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserServicePort userServicePort;
+    private final UserSecondaryPort userServicePort;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserController(UserServicePort userServicePort, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserController(UserSecondaryPort userServicePort, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userServicePort = userServicePort;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
@@ -44,5 +46,11 @@ public class UserController {
     public ResponseEntity<Void> delete(@PathVariable Long userId){
         userServicePort.delete(userId);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> update(@PathVariable Long id, @RequestBody UserUpdateDTO request){
+        UserDBO userUpdated = userServicePort.update(id, UserDBO.of(request.email(), request.password(), Set.of()));
+        return ResponseEntity.ok(UserResponseDTO.toDTO(userUpdated));
     }
 }
